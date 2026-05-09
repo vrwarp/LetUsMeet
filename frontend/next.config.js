32ac5f286c915f396e40fcd67e3f0b09575659c9
Benson Tsai <btsai@vrwarp.com>
2026-05-09 01:22:17 -0700

    maybe it works now?

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,13 @@
+const path = require('path');
+
 const nextConfig = {
   allowedDevOrigins: ['127.0.0.1'],
   output: 'standalone',
+  outputFileTracingRoot: path.join(__dirname),
 };
 
+
 module.exports = nextConfig;
+
 
 
